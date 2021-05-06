835c8124fe673c0f5c4c887b3a6aa1626543baff
cladbury <cladbury@coh.org>
2021-05-06 12:21:26 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState, useEffect } from "react";
+
+import Table from "./Table";
 
 function App() {
+
+  /* 
+    - Columns is a simple array right now, but it will contain some logic later on. It is recommended by react-table to memoize the columns data
+    - Here in this example, we have grouped our columns into two headers. react-table is flexible enough to create grouped table headers
+  */
+  const columns = useMemo(
+    () => [
+      {
+        // first group - TV Show
+        Header: "Disease Sites",
+        // First group columns
+        columns: [
+          {
+            Header: "Disease Site",
+            accessor: "score"
+          },
+          {
+            Header: "Article Link",
+            accessor: "show.name"
+          },
+          {
+            Header: "PDF",
+            accessor: "show.type"
+          }
+        ]
+      },
+    ],
+    []
+  );
+
+  const data = [
+    {
+      "score": 17.592657,
+      "show": {
+        "id": 44813,
+        "url": "http://www.tvmaze.com/shows/44813/the-snow-spider",
+        "name": "The Snow Spider",
+        "type": "Scripted",
+        "language": "English",
+        "genres": [
+          "Drama",
+          "Fantasy"
+        ],
+        "status": "In Development",
+        "runtime": 30,
+        "premiered": null,
+        "officialSite": null,
+        "schedule": {
+          "time": "",
+          "days": [
+  
+          ]
+        },
+      }
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table columns={columns} data={data} />
     </div>
   );
 }
