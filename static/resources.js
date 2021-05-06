ed8a7a7dc9e8d2ce28fe3f2e693c58f0324c87da
cladbury <cladbury@coh.org>
2021-05-06 14:33:43 -0700

    update

--- static/resources.js
+++ static/resources.js
@@ -1,25 +1,44 @@
 $('document').ready(function () {
-    db = $("#database")
-    console.log(db.html())
-    let selectedFile;
-    var data = $.get("./Database/Literature%20Database.csv");
-    //console.log(data)
-    getData(selectedFile);
+    $.get(
+        "https://cladbury.github.io/COHRadOncResidentResources/Database/Literature%20Database.csv",
+        function(data) {
+            rows = $.csv.toArrays(data).slice(2);
+            for (i in rows){
+                console.log(rows[i])
+            }
+        }
+    );
+    //getData(data);
 });
 
-function getData(selectedFile){
-    if(selectedFile){
-        let fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-         let data = event.target.result;
-         let workbook = XLSX.read(data,{type:"binary"});
-         console.log(workbook);
-         workbook.SheetNames.forEach(sheet => {
-              let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
-              console.log(rowObject);
-              document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
-         });
-        }   
-    }
+function getData(data){
+    let fileReader = new FileReader();
+    reader.onload = function(e) {
+        var data = e.target.result;
+        var workbook = XLSX.read(data, {
+          type: 'binary'
+        });
+  
+        workbook.SheetNames.forEach(function(sheetName) {
+          // Here is your object
+          var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+          var json_object = JSON.stringify(XL_row_object);
+          console.log(json_object);
+  
+        })
+  
+      };
+    //fileReader.readAsBinaryString(data);
+/* 
+    fileReader.onload = (event)=>{
+        let data = event.target.result;
+        let workbook = XLSX.read(data,{type:"binary"});
+        console.log(workbook);
+        workbook.SheetNames.forEach(sheet => {
+            let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
+            console.log(rowObject);
+            document.getElementById("jsondata").innerHTML = JSON.stringify(rowObject,undefined,4)
+        });
+    } 
+       */
 }
